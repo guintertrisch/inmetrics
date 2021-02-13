3e99aafd801d1c828d4d232bbdc3d851e02f9737
Guinter Torres Trisch <guintert@dbserver.com.br>
2021-02-13 14:55:38 -0300

    criado dataprovider

--- src/main/java/dataprovider/UsuarioProvider.java
+++ src/main/java/dataprovider/UsuarioProvider.java
@@ -1,14 +1,15 @@
 package dataprovider;
 
 import org.testng.annotations.DataProvider;
+import utils.GeradorNumeroUsuario;
 
 public class UsuarioProvider {
 
     @DataProvider(name = "dados_usuarios")
     public Object[][] createData1() {
         return new Object[][]{
-                {"Juliana123", "teste123"},
-                {"DonaVilma123", "teste123"},
+                {"Ana"+ GeradorNumeroUsuario.retornaNovoNumero(), "teste123"},
+                {"Maria"+GeradorNumeroUsuario.retornaNovoNumero(), "teste123"},
         };
     }
 
